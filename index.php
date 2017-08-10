e1f3b97e07708221f573dbd25ab6d35bf7725d0d
WyldePointer <sohrab.monfared@gmail.com>
2017-08-10 12:50:42 +0430

    Initial version.

---
+++ index.php
@@ -0,0 +1,110 @@
+<?php
+/*
+ * Copyright (c) 2017, Sohrab Monfared <sohrab.monfared@gmail.com>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *    * Redistributions of source code must retain the above copyright
+ *      notice, this list of conditions and the following disclaimer.
+ *    * Redistributions in binary form must reproduce the above copyright
+ *      notice, this list of conditions and the following disclaimer in the
+ *      documentation and/or other materials provided with the distribution.
+ *    * Neither the name of the <organization> nor the
+ *      names of its contributors may be used to endorse or promote products
+ *      derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+require "libdwrapd.php";
+require "functions_misc.php";
+
+$json = false;
+$url_array = NULL;
+$limit = 0;
+$limit_index = 0;
+$lookup_result = NULL;
+$ip_array = array();
+
+$url = url_to_array();
+
+if (!isset($url[0])){
+  return -1;
+}
+
+if (!isset($url[1])){
+  return -2;
+}
+
+$json = array_search("json", $url);
+
+$limit_index = array_search("limit", $url);
+
+/* If the word "limit" was found in URL */
+if ($limit_index){
+  $limit = 1;
+}
+
+/* If there was a number next to "limit" in URL */
+if (isset($url[$limit_index+1])){
+
+  if (intval($url[$limit_index+1]) == $url[$limit_index+1]){
+    $limit = $url[$limit_index+1];
+  }
+
+}
+
+if ($url[0] == "get_ip_by_name"){
+
+  $lookup_result = dwrapd_do_dns_lookup_a($url[1], $limit);
+
+  if (is_array($lookup_result)){
+
+    foreach ($lookup_result as $ip){
+
+      if(filter_var($ip, FILTER_VALIDATE_IP)){
+        $ip_array[] = $ip;
+      }
+
+      if (count($ip_array) >= $limit && $limit > 0){
+        break;
+      }
+
+    }
+
+  } else {
+
+    if (!empty($lookup_result)){
+      echo $lookup_result;
+    }
+
+  }
+
+  if (count($ip_array)){
+
+    if ($json){
+
+      echo json_encode($ip_array);
+
+    } else {
+
+      foreach ($ip_array as $ip){
+        echo $ip, "\n";
+      }
+
+    }
+
+  }
+
+}
+
---
+++ libdwrapd.php
@@ -0,0 +1,137 @@
+<?php
+/*
+ * Copyright (c) 2017, Sohrab Monfared <sohrab.monfared@gmail.com>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *    * Redistributions of source code must retain the above copyright
+ *      notice, this list of conditions and the following disclaimer.
+ *    * Redistributions in binary form must reproduce the above copyright
+ *      notice, this list of conditions and the following disclaimer in the
+ *      documentation and/or other materials provided with the distribution.
+ *    * Neither the name of the <organization> nor the
+ *      names of its contributors may be used to endorse or promote products
+ *      derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+function dwrapd_do_dns_lookup($hostname, $record='A'){
+
+  $result = array();
+  $php_record_type = DNS_A;
+
+  if ($hostname == ''){
+    return -1;
+  }
+
+  switch ($record){
+    case "txt":
+    case "TXT":
+      $php_record_type = DNS_TXT;
+      break;
+
+    case 'a':
+    case 'A':
+      $php_record_type = DNS_A;
+      break;
+
+    default:
+      return -13; /* DWRAPD_LOOKUP_NOT_IMPLEMENTED_FOR_RECORD */
+  }
+
+  $result = dns_get_record($hostname, $php_record_type);
+
+  if (isset($result[0]["type"])){
+
+    switch ($result[0]["type"]){
+
+      case "TXT":
+        return $result[0]["txt"];
+        break;
+
+      case 'A':
+        return $result[0]["ip"];
+        break;
+
+    }
+
+  }
+
+  return 0;
+}
+
+
+function dwrapd_do_dns_lookup_a($hostname, $limit=0){
+
+  $ips = NULL;
+
+  /*
+   *  TODO: Timeout for gethostbyname functions.
+   */
+  if ($limit == 1){
+    $ips = gethostbyname($hostname);
+  }else{
+    $ips = gethostbynamel($hostname);
+  }
+
+  return $ips;
+}
+
+
+function dwrapd_do_dns_lookup_mx($hostname){
+
+  $mx_records = array();
+  $weights = array();
+  $formatted = array();
+
+  /*
+   *  TODO: Finding the most address-friendly regex
+   *        and using it instead of doing the actual lookup.
+   */
+
+  if(!filter_var(dwrapd_do_dns_lookup_a($hostname, 1), FILTER_VALIDATE_IP)){
+    return -1;
+  }
+
+  if (getmxrr($hostname, $mx_records, $weights)){
+
+    if (count($mx_records) == count($weights)){
+
+      foreach ($weights as $key => $value){
+
+        /*
+         *  TODO: Making sure the returned address is not spoofed.
+         *        (e,g. it's a valid record)
+         */
+
+        if (isset($mx_records[$key])){
+          $formatted[$value] = $mx_records[$key];
+        }
+
+      }
+
+      if (count($formatted)>0){
+        return $formatted;
+      }
+
+    }
+
+    return $mx_records;
+  }
+
+
+  return 0;
+}
+
+
